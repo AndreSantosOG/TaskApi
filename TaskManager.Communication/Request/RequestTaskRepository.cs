09755ebfc5ac580085eeca3f5ba22cc2f85f7180
André Santos <andreluissouzasantos2711@gmail.com>
2024-05-02 13:22:18 -0300

    Add files via upload

---
+++ TaskManager.Communication/Request/RequestTaskRepository.cs
@@ -0,0 +1,28 @@
+﻿namespace TaskManager.Communication.Request;
+
+public class RequestTaskRepository
+{
+    private static List<RequestTaskJson> Task = new List<RequestTaskJson>();
+
+    public void AddTask(RequestTaskJson works)
+    {
+        works.Id = Task.Count + 1;
+        Task.Add(works);
+    }
+    public List<RequestTaskJson> GetAllTasks()
+    {
+        return Task;
+    }
+    public RequestTaskJson GetTasksById(int id)
+    {
+        return Task.FirstOrDefault(works => works.Id == id);
+    }
+    public void RemoveTask(int id)
+    {
+        var taskToRemove = Task.FirstOrDefault(works => works.Id == id);
+        if (taskToRemove != null)
+        {
+            Task.Remove(taskToRemove);
+        }
+    }
+}
---
+++ TaskManager.Communication/Response/ResponseRegisteredTaskJson.cs
@@ -0,0 +1,15 @@
+﻿using TaskManager.Communication.Enums;
+using TaskStatus = TaskManager.Communication.Enums.TaskStatus;
+
+
+namespace TaskManager.Communication.Response;
+
+public class ResponseRegisteredTaskJson
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public TaskType Type { get; set; }
+    public DateTime DateLimit { get; set; }
+    public TaskStatus Status { get; set; }
+}
